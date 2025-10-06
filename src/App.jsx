3c8ea78181ec5d2aab669417743bee29ce02c10f
Nguyen Nhan Thieu <23521492@gm.uit.edu.vn>
2025-10-06 21:31:26 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello from ReactJS!</h1>
+      <p>This is my React web deployed on Vercel 🚀</p>
+    </div>
+  );
+}
+
+export default App;
+
